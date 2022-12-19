f22b2f632e56ef250f889564e7dd6e04b6762cd3
Chunky <jasonc@nulogy.com>
2022-12-18 22:49:13 -0500

    refactor

--- Ruby/lib/parrot.rb
+++ Ruby/lib/parrot.rb
@@ -1,35 +1,37 @@
-class Parrot
-  def initialize type, number_of_coconuts, voltage, nailed
-    @type = type;
-    @number_of_coconuts = number_of_coconuts;
-    @voltage = voltage;
-    @nailed = nailed;
-  end
+ParrotType = Struct.new(:type, :number_of_coconuts, :voltage, :nailed)
 
+class EuropeanParrot < ParrotType
   def speed
-    case @type
-    when :european_parrot
-      return base_speed
-    when :african_parrot
-      return [0, base_speed - load_factor * @number_of_coconuts].max
-    when :norwegian_blue_parrot
-      return (@nailed) ? 0 : compute_base_speed_for_voltage(@voltage);
-    end
+    12
+  end
+end
 
-    throw "Should be unreachable!";
+class AfricanParrot < ParrotType
+  def speed
+    [12 - (9 * number_of_coconuts), 0].max
+  end
+end
+
+class NorwegianBlueParrot < ParrotType
+  def speed
+    [voltage * 12, 24].min
+  end
+end
+
+class Parrot < ParrotType
+  def speed
+    return 0 if nailed
+
+    parrot_type.speed
   end
 
   private
 
-  def compute_base_speed_for_voltage voltage
-   [24.0, voltage * base_speed].min
+  def parrot_type
+    case type
+    when :european_parrot then EuropeanParrot
+    when :african_parrot then AfricanParrot
+    when :norwegian_blue_parrot then NorwegianBlueParrot
+    end.new(*values)
   end
-
-  def load_factor
-    9.0;
-  end
-
-  def base_speed
-    12.0;
-  end  
 end
