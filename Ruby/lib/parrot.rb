387e59660863287d477fa8e1b9a84ff65aaaad6a
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-20 06:19:17 -0400

    refactor

--- Ruby/lib/parrot.rb
+++ Ruby/lib/parrot.rb
@@ -7,14 +7,10 @@ class Parrot < Struct.new(:type, :number_of_coconuts, :voltage, :nailed)
 
   def base_speed
     case type
-    when :european_parrot
-      12
-    when :african_parrot
-      12 - (9 * number_of_coconuts)
-    when :norwegian_blue_parrot
-      voltage * 12
-    else
-      raise "Unknown type: #{type}"
+    when :european_parrot then 12
+    when :african_parrot then 12 - (9 * number_of_coconuts)
+    when :norwegian_blue_parrot then voltage * 12
+    else raise "Unknown type: #{type}"
     end
   end
 end
